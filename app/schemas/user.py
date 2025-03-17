55e3a594d5abc9e4ad4efa7692dd5ccdb8597408
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-03-17 02:48:15 +0100

    Add Pydantic models for user schema including validation and response formats

---
+++ app/schemas/user.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, EmailStr, Field, constr
+from uuid import UUID
+from datetime import datetime
+from typing import Optional, Union
+import re
+
+pattern = r"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$"
+
+
+class UserBase(BaseModel):
+    email: EmailStr = Field(title="Email Address")
+    first_name: str = Field(title="First Name")
+    last_name: str = Field(title="Last Name")
+    middle_name: Union[str, None] = Field(title="Middle Name")
+    phone_number: Union[str, None] = Field(title="Phone Number")
+    nin: str = Field(title="National Identification Number")
+    is_identical: bool = Field(title="Is Identical")
+    photo_image: Optional[str] = Field(title="Photo Image")
+    government_id: Optional[str] = Field(title="Government ID")
+    thumb_photo: Optional[str] = Field(title="Thumb Photo")
+
+
+class UserCreate(UserBase):
+    hashed_password: str = Field(title="Hashed Password")
+
+
+class UserResponse(UserBase):
+    id: UUID
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
