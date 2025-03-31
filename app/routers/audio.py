3879a2b2544222fca767dc337232f65872428ef4
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-03-31 15:37:13 +0100

    Refactor audio status responses to use BaseResult for consistency; enhance endpoint summaries for clarity

--- app/routers/audio.py
+++ app/routers/audio.py
@@ -53,14 +53,19 @@ async def upload_audio(background_tasks: BackgroundTasks, file: UploadFile = Fil
 
     background_tasks.add_task(process_audio_background, audio_bytes, task_id)
 
-    return {"status": "processing", "task_id": task_id}
+    return BaseResult(
+        status=status.HTTP_200_OK,
+        message="Message Sent to queue",
+        data={"status": "processing", "task_id": task_id},
+    )
 
 
-@router.get("/test/status/{task_id}")
+@router.get(
+    "/test/status/{task_id}",
+    summary="Fetches the processing status of an audio verification task from in-memory.",
+)
 async def get_audio_status(task_id: str):
-    """
-    Fetches the processing status of an audio verification task from in-memory.
-    """
+
     # Check if task exists
     if task_id not in task_store:
         raise HTTPException(status_code=404, detail=f"Task with ID {task_id} not found")
@@ -68,22 +73,38 @@ async def get_audio_status(task_id: str):
     task_result = task_store[task_id]
 
     if task_result["status"] == "PENDING":
-        return {"status": "Processing...", "task_id": task_id}
+        return BaseResult(
+            status=status.HTTP_200_OK,
+            message="Audio Processing",
+            data={"status": "Processing...", "task_id": task_id},
+        )
     elif task_result["status"] == "RUNNING":
-        return {"status": "Processing...", "task_id": task_id}
+        return BaseResult(
+            status=status.HTTP_200_OK,
+            message="Audio Processing",
+            data={"status": "Processing...", "task_id": task_id},
+        )
     elif task_result["status"] == "SUCCESS":
-        return {"status": "Completed", "result": task_result["result"]}
+        return BaseResult(
+            status=status.HTTP_200_OK,
+            message="Audio Processing succeed",
+            data={"status": "Completed", "result": task_result["result"]},
+        )
     elif task_result["status"] == "FAILURE":
-        return {"status": "Failed", "error": task_result["error"]}
-
-    return {"status": "Unknown", "task_id": task_id}
+        return BaseResult(
+            status=status.HTTP_200_OK,
+            message="Audio Processing Failed",
+            data={"status": "Failed", "error": task_result["error"]},
+        )
+    return BaseResult(
+        status=status.HTTP_200_OK,
+        message="Audio Processing Failed",
+        data={"status": "Unknown", "task_id": task_id},
+    )
 
 
 @router.post("/upload-audio", summary="Verify an audio file using celery and redis")
 async def verify_audio(file: UploadFile = File(...)):
-    """
-    Endpoint to verify an uploaded audio file.
-    """
 
     if file.content_type.split("/")[0] != "audio":
         raise HTTPException(
@@ -116,11 +137,11 @@ async def verify_audio(file: UploadFile = File(...)):
     )
 
 
-@router.get("/status/{task_id}")
+@router.get(
+    "/status/{task_id}",
+    summary="Fetches the processing status of an audio verification task from using celery and redis.",
+)
 async def get_audio_status(task_id: str):
-    """
-    Fetches the processing status of an audio verification task from using celery and redis.
-    """
 
     task_result = AsyncResult(task_id)
 
